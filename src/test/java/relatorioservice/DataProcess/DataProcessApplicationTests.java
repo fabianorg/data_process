2cfb74f8b60a6eeb3eaed530f270c75fa8bb8df6
Fabiano <fabianorg@gmail.com>
2020-10-09 16:24:05 -0300

    Arquivos iniciais do projeto

---
+++ src/test/java/relatorioservice/DataProcess/DataProcessApplicationTests.java
@@ -0,0 +1,13 @@
+package relatorioservice.DataProcess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataProcessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
